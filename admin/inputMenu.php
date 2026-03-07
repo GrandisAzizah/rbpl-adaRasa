f492bfb8118da2d3466476816caecb184caec34b
GrandisAzizah <124240045@student.upnyk.ac.id>
2026-03-07 14:15:32 +0700

    add msg error

--- admin/inputMenu.php
+++ admin/inputMenu.php
@@ -7,19 +7,21 @@
 //     exit;
 // }
 
-require 'functions.php';
-// cek apakah tombol sudah dipencet
+require '../functions.php';
+
+// alert jika input berhasil atau gagal
+$pesan = '';
+$tipe = '';
 if (isset($_POST["submit"])) {
-    // cek apakah data berhasil ditambahkan atau tidak
-    if (tambah($_POST) > 0) {
-        echo "Data berhasil ditambahkan";
+    $hasil = tambah($_POST);
+    if ($hasil > 0) {
+        $pesan = 'Data berhasil ditambahkan!';
+        $tipe = 'success';
     } else {
-        echo "Data gagal ditambahkan";
-        // menampilkan kenapa gagal
-        echo mysqli_error($conn);
+        $pesan = 'Data gagal ditambahkan!';
+        $tipe = 'danger';
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -28,29 +30,145 @@ if (isset($_POST["submit"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Aleo:wght@300;400;600;700&display=swap" rel="stylesheet">
     <title>Tambah Data</title>
 </head>
 
+
+<style>
+    body {
+        font-family: 'Aleo', serif;
+        background-color: #ffffff;
+        margin: 0;
+        padding: 20px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .container {
+        width: 290px;
+        margin: 0 auto;
+        padding: 20px;
+        border-radius: 8px;
+        outline: black solid 1px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .alert {
+        width: 290px;
+        margin: 0 auto;
+        padding: 20px;
+        border-radius: 8px;
+        outline: black solid 1px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .btn-close {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        box-shadow: none !important;
+    }
+
+    button {
+        width: 100%;
+        padding: 4px !important;
+    }
+
+    input {
+        width: 100%;
+        padding: 4px;
+        margin-top: 5px;
+        border: 1px solid #B3B3B3;
+        border-radius: 4px;
+    }
+
+    .menu-item {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        width: 290px;
+    }
+
+    .menu-item .icon {
+        flex-shrink: 0;
+        width: 24px;
+    }
+
+    h5 {
+        font-weight: 600;
+        margin-top: 20px;
+        text-align: center;
+    }
+
+    label {
+        font-weight: 600;
+        margin-top: 5px;
+    }
+
+    h1 {
+        text-align: center;
+        margin-bottom: 20px;
+        margin-top: 20px;
+    }
+
+    input:focus {
+        outline: 1px solid #B3B3B3 !important;
+        border-color: #B3B3B3 !important;
+        box-shadow: none !important;
+    }
+</style>
+
 <body>
-    <h1>Tambah Data</h1>
-    <form action="" method="POST" enctype="multipart/form-data">
+    <div>
+        <div style="display: flex; align-items: center; justify-content: center; position: relative;" class="mb-3">
+            <a href="menu.php" style="position: absolute; left: 0; flex-shrink: 0;">
+                <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M31.6667 19H6.33337M6.33337 19L15.8334 9.5M6.33337 19L15.8334 28.5" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </a>
+            <h5 style="margin: 0;">Tambah Data</h5>
+        </div>
+        <div class="container">
+            <form action="" method="POST" enctype="multipart/form-data">
 
-        <!-- INPUT NAMA MENU -->
-        <label for="nama-menu">Nama Menu <br></label>
-        <input type="text" name="nama-menu" id="nama-menu" required><br><br>
+                <!-- INPUT NAMA MENU -->
+                <label for="nama-menu">Nama Menu <br></label>
+                <input type="text" name="nama-menu" id="nama-menu" maxlength="20" required><br><br>
 
-        <!-- INPUT HARGA -->
-        <label for="harga-menu">Harga Menu:<br></label>
-        <input type="text" name="harga-menu" id="harga-menu"><br><br>
+                <!-- INPUT HARGA -->
+                <label for="harga-menu">Harga Menu:<br></label>
+                <input type="number" name="harga-menu" id="harga-menu" min="0" max="999999" required><br><br>
 
-        <!-- INPUT GAMBAR -->
-        <label for="gambar-menu">Gambar:<br></label>
-        <input type="file" name="gambar-menu" id="gambar-menu"><br><br>
+                <!-- INPUT GAMBAR -->
+                <label for="gambar-menu">Gambar:<br></label>
+                <input type="file" name="gambar-menu" id="gambar-menu" required><br><br>
 
-        <!-- SUBMIT BUTTON -->
-        <button type="submit" value="Kirim" name="submit">Kirim</button>
-
-    </form>
+                <!-- SUBMIT BUTTON -->
+                <button type="submit" value="Kirim" name="submit" class="btn btn-dark mt-3">Kirim</button>
+            </form>
+        </div>
+        <?php if ($pesan): ?>
+            <div class="mt-3 alert alert- <?= $tipe ?> alert-dismissible fade show" role="alert">
+                <?= $pesan ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            </div>
+        <?php endif; ?>
+    </div>
 </body>
 
 </html>
+
+<script>
+    document.getElementById('harga-menu').addEventListener('input', function() {
+        if (this.value > 999999) {
+            this.value = this.value.slice(0, -1); // hapus karakter terakhir
+        }
+    });
+</script>
