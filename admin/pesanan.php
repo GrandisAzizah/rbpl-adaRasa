4161218d803683567410813859eb48cacc5a5faf
GrandisAzizah <124240045@student.upnyk.ac.id>
2026-03-14 01:49:57 +0700

    initial commit

--- admin/pesanan.php
+++ admin/pesanan.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+// user belum login
+if (!isset($_SESSION["login"])) {
+    header("location: login.php");
+    exit;
+}
+
+require '../functions.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,10 +16,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pesanan & Pelanggan</title>
+    <link rel="stylesheet" href="pesanan.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Aleo:wght@300;400;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    Pesanan
+    <div class="container-main">
+        <div class="header-nav mt-3">
+            <a href="berandaAdmin.php" class="header-nav-left">
+                <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M31.6667 19H6.33337M6.33337 19L15.8334 9.5M6.33337 19L15.8334 28.5" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </a>
+            <h5 class="header-nav-title" style="margin: 0;">Pesanan</h5>
+            <a href="" class="header-nav-right">
+                <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.91669 18.9998H30.0834M19 7.9165V30.0832" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </a>
+        </div>
+    </div>
+
+    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+        <label class="btn btn-outline-primary" for="btnradio1">Pesanan</label>
+
+        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+        <label class="btn btn-outline-primary" for="btnradio2">Pelanggan</label>
+    </div>
 </body>
 
 </html>
